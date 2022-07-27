98dd1324c717a2e869d1775fcb1da084939976d1
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-07-28 01:09:27 +0300

    Level-2 header v1_1 nav wv

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation, Link } from 'react-router-dom';
 
 function Navigation() {
   const location = useLocation();
   const [isNavOpened, setIsNavOpened] = useState(false);
+
+  const handleNavOpen = () => {
+    setIsNavOpened(true);
+  }
+
+  const handleNavClose = () => {
+    setIsNavOpened(false);
+  }
+
   console.log(location.pathname);
+
+  const auth = location.pathname !== ('/signin' || '/signup');
 
   return (
     <div className='navigation'>
@@ -16,15 +27,18 @@ function Navigation() {
           </Link>
         </nav>
       }
-      {(location.pathname === ('/movies' || '/saved-movies' || '/profile')) &&
-        <nav className={isNavOpened ? 'navigation__bar navigation__bar_type_auth-page navigation__bar_displayed' : 'navigation__bar navigation__bar_type_auth-page'}>
-          <Link to={'/movies'} className='navigation__link navigation__link_type_movies'>Фильмы</Link>
-          <Link to={'/saved-movies'} className='navigation__link navigation__link_type_saved-movies'>Сохраненные фильмы</Link>
-          <Link to={'/profile'} className='navigation__link navigation__link_type_profile'>
-            <span>Аккаунт</span>
-            <button className='navigation__account-button'></button>
-          </Link>
-          <button className={isNavOpened ? 'navigatiion__open-button' : 'navigatiion__open-button navigation__open-button_displayed'}></button>
+      {(location.pathname !== '/' && (location.pathname !== '/signin') && (location.pathname !== '/signup')) &&
+        <nav className='navigation__bar navigation__bar_type_auth-page'>
+          <ul className={isNavOpened ? 'navigation__list navigation__list_opened' : 'navigation__list'}>
+            <button className={isNavOpened ? 'navigatiion__close-button' : 'navigatiion__close-button navigation__close-button_displayed'} onClick={handleNavClose} ></button>
+            <Link to={'/movies'} className='navigation__link navigation__link_type_movies'>Фильмы</Link>
+            <Link to={'/saved-movies'} className='navigation__link navigation__link_type_saved-movies'>Сохраненные фильмы</Link>
+            <Link to={'/profile'} className='navigation__link navigation__link_type_profile' onClick={handleNavClose}>
+              <span>Аккаунт</span>
+              <button className='navigation__account-button' onClick={handleNavClose}></button>
+            </Link>
+          </ul>
+          <button className={isNavOpened ? 'navigatiion__open-button' : 'navigatiion__open-button navigation__open-button_opened'} onClick={handleNavOpen} ></button>
         </nav>
       }
     </div>
