e267843ddc5760f0d3ea5c3f04443afef4b64438
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-07-28 04:15:25 +0300

    Level-2 header v1_2

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -21,7 +21,9 @@ function Navigation() {
     <div className='navigation'>
       {(location.pathname === '/') &&
         <nav className='navigation__bar navigation__bar_type_main-page'>
-          <Link to={'/signup'} className='navigation__link navigation__link_type_register'>Регистрация</Link>
+          <Link to={'/signup'} className='navigation__link navigation__link_type_register'>
+            <span className='navigation__link-name'>Регистрация</span>
+          </Link>
           <Link to={'/signin'} className='navigation__link navigation__link_type_login'>
             <button className='navigation__login-button'>Войти</button>
           </Link>
@@ -31,14 +33,19 @@ function Navigation() {
         <nav className='navigation__bar navigation__bar_type_auth-page'>
           <ul className={isNavOpened ? 'navigation__list navigation__list_opened' : 'navigation__list'}>
             <button className={isNavOpened ? 'navigatiion__close-button' : 'navigatiion__close-button navigation__close-button_displayed'} onClick={handleNavClose} ></button>
-            <Link to={'/movies'} className='navigation__link navigation__link_type_movies'>Фильмы</Link>
-            <Link to={'/saved-movies'} className='navigation__link navigation__link_type_saved-movies'>Сохраненные фильмы</Link>
+            <Link to={'/movies'} className='navigation__link navigation__link_type_movies'>
+              <span className='navigation__link-name'>Фильмы</span>
+            </Link>
+            <Link to={'/saved-movies'} className='navigation__link navigation__link_type_saved-movies'>
+              <span className='navigation__link-name'>Сохраненные фильмы</span>
+            </Link>
             <Link to={'/profile'} className='navigation__link navigation__link_type_profile' onClick={handleNavClose}>
-              <span>Аккаунт</span>
+              <span className='navigation__link-name'>Аккаунт</span>
               <button className='navigation__account-button' onClick={handleNavClose}></button>
             </Link>
           </ul>
           <button className={isNavOpened ? 'navigatiion__open-button' : 'navigatiion__open-button navigation__open-button_opened'} onClick={handleNavOpen} ></button>
+          <div className={isNavOpened ? 'navigation__overlay navigation__overlay_opened' : 'navigation__overlay'}></div>
         </nav>
       }
     </div>
