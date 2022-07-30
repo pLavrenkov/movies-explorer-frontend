e41456736f058b02c3318d93570c351990f49a12
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-07-30 18:21:10 +0300

    Level-2 footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,24 @@
+import { Link } from 'react-router-dom';
+
 function Footer() {
   return (
-    <div>footer</div>
+    <footer className='footer'>
+      <p className='footer__head'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <div className='footer__link-bar'>
+        <span className='footer__copyright'>&copy; 2022</span>
+        <ul className='footer__nav'>
+          <li>
+            <a href='https://practicum.yandex.ru/' className='footer__link'>Яндекс.Практикум</a>
+          </li>
+          <li>
+            <a href='https://github.com' className='footer__link'>GitHub</a>
+          </li>
+          <li>
+            <a href='https://vk.com/' className='footer__link'>ВКонтакте</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   )
 }
 
