dbbe56fb2ec82f0320f425ab57d29321e781c467
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-07-31 08:26:31 +0300

    Level-2 login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,32 @@
+import { Link } from 'react-router-dom';
+
 import Header from "../Header/Header";
-import Navigation from "../Navigation/Navigation";
+import Form from "../Form/Form";
 
 function Login() {
+  const nonAuthClassHeader = 'header header_type_non-auth';
+
   return (
-    <>
-      <Header />
-      <div>login</div>
-    </>
+    <section className='login'>
+      <Header classNonAuth={nonAuthClassHeader} />
+      <h1 className='login__title'>Рады видеть!</h1>
+      <Form name={'login'} button={'Зарегистрироваться'}>
+        <p className='form__set'>
+          <label htmlFor='login-email' className='form__label'>E-mail</label>
+          <input id='login-email' type='email' className='form__input'></input>
+          <span className='form__error'></span>
+        </p>
+        <p className='form__set'>
+          <label htmlFor='login-password' className='form__label'>Пароль</label>
+          <input id='login-password' type='password' className='form__input'></input>
+          <span className='form__error'>Ошибка</span>
+        </p>
+      </Form>
+      <nav className='login__nav'>
+        <p className='login__subtitle'>Еще не зарегистрированы?</p>
+        <Link to={'/signup'} className='login__link'>Регистрация</Link>
+      </nav>
+    </section>
   )
 }
 
