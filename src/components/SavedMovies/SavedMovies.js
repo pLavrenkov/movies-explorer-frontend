a57f0897f94644811da5de1f9dc0da44a2853094
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-08-11 21:38:07 +0300

    Level-3 saved movies handle&validation

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,15 +1,28 @@
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import { useState } from "react";
 
 import { moviesPath, savedMoviesBD } from "../../utils/moviesBD";
+import  moviesFilter  from "../../utils/movies-filter";
 
 function SavedMovies() {
+  const [isShortMovie, setIsShortMovie] = useState(false);
+  const [reqMovies, setReqMovies] = useState('');
+
+  const toggleShortMovie = () => {
+    setIsShortMovie(!isShortMovie);
+  }
+
+  const handleMoviesRequest = (req) => {
+    setReqMovies(req);
+  }
+
+  const movies = moviesFilter(savedMoviesBD, isShortMovie, ...reqMovies.split(/[\s,.-]+/))
+
   return (
     <section className="saved-movies">
-      <SearchForm />
-      <MoviesCardList movies={savedMoviesBD} savedMovies={[]} moviesPath={moviesPath} />
+      <SearchForm isShort={toggleShortMovie} handleReq={handleMoviesRequest}/>
+      <MoviesCardList movies={movies} savedMovies={[]} moviesPath={moviesPath} />
     </section>
   )
 }
